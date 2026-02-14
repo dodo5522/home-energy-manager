dodo5522/study-rust-be

[domain] Add GenerationRepository interface

layers/domain/src/entity/mod.rs
@@ -1,0 +1,2 @@
+mod generation;
+pub use generation::*;

dodo5522/home-energy-manager

layers/domain/src/entity/mod.rs
@@ -1,0 +1,10 @@
+mod group;
+pub use group::*;
+mod history;
+pub use history::*;
+mod label;
+pub use label::*;
+mod source;
+pub use source::*;
+mod unit;
+pub use unit::*;
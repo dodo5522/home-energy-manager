f26de56f058719f21cf9f44d1a3b6ffee00de6db
Takashi Ando <dodo5522@gmail.com>
2026-03-07 11:28:18 +0900

    [presentation] Allow origins on router's layer

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -14,12 +14,18 @@ async fn run() -> anyhow::Result<()> {
     let bind_addr = var("BIND_ADDR")?;
     let bind_port = var("BIND_PORT")?;
     let address = format!("{bind_addr}:{bind_port}");
+    let allowed_origins: Vec<String> = var("ALLOWED_ORIGINS")?
+        .split(",")
+        .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
+        .map(|s| s.into())
+        .collect();
 
     tracing_subscriber::fmt::init();
 
     // run our app with hyper, listening globally on the port
     let listener = tokio::net::TcpListener::bind(address).await?;
-    axum::serve(listener, routers::route()).await?;
+    axum::serve(listener, routers::route(allowed_origins)).await?;
 
     Ok(())
 }
